eda5f8ba61a041f93f16466e8fbc2f22bce8c0b4
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-02-06 21:02:33 +0530

    Reading notes and reusability

--- notes.js
+++ notes.js
@@ -35,7 +35,9 @@ var getAll = () => {
 };
 
 var getNote = title => {
-  console.log('Getting note', title);
+  let notes = fetchNotes();
+  var singleNote = notes.filter(note => note.title === title);
+  return singleNote[0];
 };
 
 var removeNote = title => {
@@ -45,9 +47,16 @@ var removeNote = title => {
   return notes.length !== filteredNotes.length;
 };
 
+var logNote = note => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
